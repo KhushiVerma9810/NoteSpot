9ef25e987e7036e046cb4fc04c049ebfdf7834db
Khushi Verma <khushiverma54583@gmail.com>
2023-03-04 14:47:16 +0530

    install a react-router-dom concurrently and create a navbar,home,about page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
+ import { About } from './components/About';
+ import Home from './components/Home';
+// Set up a router
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <BrowserRouter>
+    <Navbar/>
+    <div class="mt-20">
+    {/* <AlertProvider> */}
+    <div className="container">
+      <Routes>
+      <Route  exact path="/" element = {<Home />}>
+               
+            </Route>
+            <Route exact path="/about"  element={<About/>} >
+            
+            </Route>
+            {/* <Route exact path="/login"  element={<Login />} >
+            
+            </Route>
+            <Route exact path="/signup"  element={<SignUp />} >
+            
+            </Route> */}
+          </Routes>
+          </div>
+          {/* </AlertProvider> */}
+          </div>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+     this is my home
+      </div>
+  )
+      }
+export default Home
